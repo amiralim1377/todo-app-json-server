8d08127e1614e5f27232c3527f9cb2d5499bac35
AMIRALI <alimoradi7718@gmail.com>
2026-05-04 19:51:54 +0330

    sort array base on completed

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import { TaskListItem } from "../TaskListItem";
 import { apiFetch } from "../../services/apiFetch";
 import type { ITodo } from "../../types/Todo.interface";
+import { NoTasks } from "../NoTasks";
 
 function TodoList() {
   const [todosArray, setTodoArray] = useState<Array<ITodo>>([]);
+  const sortedTodo = [...todosArray].sort(
+    (a, b) => Number(a.completed) - Number(b.completed),
+  );
   useEffect(() => {
     const getData = async () => {
       try {
@@ -18,15 +22,20 @@ function TodoList() {
     };
     getData();
   }, []);
+
   return (
     <div className="mt-2 w-full">
       <h1 className="text-left text-xl text-zinc-600 capitalize md:text-4xl">
         task
       </h1>
-      <div className="overflow-y-scroll">
-        {todosArray?.map((todoItem: ITodo) => {
-          return <TaskListItem key={todoItem.id} todoItem={todoItem} />;
-        })}
+      <div className="max-h-64 overflow-y-scroll">
+        {todosArray.length > 0 ? (
+          sortedTodo?.map((todoItem: ITodo) => {
+            return <TaskListItem key={todoItem.id} todoItem={todoItem} />;
+          })
+        ) : (
+          <NoTasks />
+        )}
       </div>
     </div>
   );
