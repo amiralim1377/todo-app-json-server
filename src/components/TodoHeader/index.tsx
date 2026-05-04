a60285fc0c2d57675d365aa611b08394a317b8f4
AMIRALI <alimoradi7718@gmail.com>
2026-05-04 20:53:01 +0330

    change header font-size

--- src/components/TodoHeader/index.tsx
+++ src/components/TodoHeader/index.tsx
@@ -1,6 +1,6 @@
 function TodoHeader() {
   return (
-    <div className="header-image flex items-center justify-center text-6xl font-bold text-white capitalize">
+    <div className="header-image flex items-center justify-center text-4xl font-bold text-white capitalize md:text-6xl">
       todo app
     </div>
   );
