1602bc7385b67c2f648582e0ae685dfdf2021f12
AMIRALI <alimoradi7718@gmail.com>
2026-05-04 11:05:38 +0330

    imp todo form

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ export default function App() {
   return (
     <div className="felx min-h-screen w-full items-center justify-center">
       <TodoHeader />
-      <AddTodoForm />
+      <div className="mx-auto flex w-full items-center justify-center p-2 md:p-6 xl:w-2/3">
+        <AddTodoForm />
+      </div>
     </div>
   );
 }
--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -1,5 +1,75 @@
+import React, { useId, useRef } from "react";
+import { apiFetch } from "../../services/apiFetch";
+
+type TodoObjType = {
+  todo: string;
+  completed: boolean;
+  dueDate: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 function AddTodoForm() {
-  return <div>AddTodoForm</div>;
+  const todoRef = useRef<HTMLInputElement>(null);
+  const dueDateRef = useRef<HTMLInputElement>(null);
+  const todoId = useId();
+  const dateId = useId();
+  const today = new Date().toISOString().split("T")[0];
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault();
+    const todoValue = todoRef.current!.value;
+    const dueValue = dueDateRef.current!.value;
+
+    const todoObj: TodoObjType = {
+      todo: todoValue,
+      completed: false,
+      dueDate: dueValue,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    apiFetch("http://localhost:4000/todo", "POST", todoObj)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-full flex-col items-center gap-1 p-2 md:flex-row md:space-x-6"
+    >
+      <div className="flex w-full flex-col gap-1 text-zinc-500 capitalize">
+        <label className="hidden md:block" htmlFor={todoId}>
+          write your todo below
+        </label>
+        <input
+          id={todoId}
+          ref={todoRef}
+          type="text"
+          placeholder="add Todo here"
+          className="rounded-md border border-zinc-600 px-2 py-2 placeholder:text-zinc-500"
+        />
+      </div>
+      <div className="flex w-full flex-col gap-1 text-zinc-500 capitalize">
+        <label className="hidden text-nowrap md:block" htmlFor={dateId}>
+          when should Todo be Done?
+        </label>
+        <input
+          id={dateId}
+          type="date"
+          ref={dueDateRef}
+          min={today}
+          className="w-full rounded-md border border-zinc-600 px-4 py-2 placeholder:text-zinc-500"
+        />
+      </div>
+      <button
+        type="submit"
+        className="hover:bg-secondary bg-primary mt-0 w-full cursor-pointer rounded-md px-4 py-2 text-white capitalize transition-colors md:mt-6 md:w-1/2"
+      >
+        add
+      </button>
+    </form>
+  );
 }
 
 export { AddTodoForm };
