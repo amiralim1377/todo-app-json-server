47ef19ad1ad96e92c465f81cd72d47b5cb36267b
AMIRALI <alimoradi7718@gmail.com>
2026-05-04 19:50:00 +0330

    notasks components

---
+++ src/components/NoTasks/index.tsx
@@ -0,0 +1,9 @@
+function NoTasks() {
+  return (
+    <div className="text-primary h flex w-full items-center justify-center overflow-hidden rounded-md border border-zinc-500 p-6">
+      No tasks were added.
+    </div>
+  );
+}
+
+export { NoTasks };
