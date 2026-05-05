15bee1ed6416587b60a9400cabcbc6b7169a788d
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 14:00:01 +0330

    replace with axios req-func

--- src/components/TaskListItem/index.tsx
+++ src/components/TaskListItem/index.tsx
@@ -1,5 +1,9 @@
 import { useWindowSize } from "../../hook/useWindowSize";
-import { apiFetch } from "../../services/apiFetch";
+import {
+  doneHandler,
+  getTodo,
+  removeTodo,
+} from "../../services/Axios/Requests/todos/Todo";
 import type { ITodo } from "../../types/Todo.interface";
 import { classNames } from "../../utility/classNames";
 import { getRandomEmoji } from "../../utility/getRandomEmoji";
@@ -21,8 +25,7 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
 
   const handleDeleteTodo = async (id: number) => {
     try {
-      const data = await apiFetch(`http://localhost:4000/todo/${id}`, "DELETE");
-      console.log(data);
+      await removeTodo(id);
     } catch (error) {
       console.log(error);
     }
@@ -30,17 +33,12 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
 
   const handleDoneTodo = async (todoId: number) => {
     try {
-      const todoData = await apiFetch("http://localhost:4000/todo", "GET");
+      const { data: todoData } = await getTodo();
       const lookingTodo = todoData.find((todo: ITodo) => todo.id == todoId);
-      console.log(lookingTodo);
-      const newTodo = { ...lookingTodo, completed: true };
-      console.log(newTodo);
-      const sendNewTodo = await apiFetch(
-        `http://localhost:4000/todo/${todoId}`,
-        "PUT",
-        newTodo,
-      );
-      console.log(sendNewTodo);
+      if (lookingTodo) {
+        const newTodo = { ...lookingTodo, completed: true };
+        await doneHandler(todoId, newTodo);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -65,32 +63,35 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
         </span>
         <span>{completed ? "" : getTimeDifference(dueDate)}</span>
       </div>
+      {!completed && (
+        <button
+          onClick={() => handleDoneTodo(id as number)}
+          disabled={completed}
+          className={classNames(
+            completed
+              ? "border border-t-transparent border-b-transparent bg-gray-200 text-gray-500"
+              : "bg-amber-300 text-wrap text-black hover:bg-amber-200",
+            "cursor-pointer p-1 text-xs capitalize transition-all md:p-3",
+          )}
+        >
+          {isCompleted}
+        </button>
+      )}
 
       <button
-        onClick={() => handleDoneTodo(id)}
         disabled={completed}
         className={classNames(
           completed
-            ? "border border-t-transparent border-b-transparent bg-gray-200 text-gray-500"
-            : "bg-amber-300 text-wrap text-black hover:bg-amber-200",
-          "cursor-pointer p-1 text-xs capitalize transition-all md:p-3",
-        )}
-      >
-        {isCompleted}
-      </button>
-      <button
-        disabled={completed}
-        className={classNames(
-          completed
-            ? "bg-gray-200 text-gray-500"
-            : "bg-accent hover:bg-accent/80 text-white",
-          "cursor-pointer p-1 text-sm capitalize md:p-3",
+            ? "cursor-not-allowed border-l border-l-zinc-500 bg-gray-200 text-gray-500"
+            : "bg-accent hover:bg-accent/80 cursor-pointer text-white",
+          "p-1 text-sm capitalize md:p-3",
         )}
       >
         edit
       </button>
+
       <button
-        onClick={() => handleDeleteTodo(id)}
+        onClick={() => handleDeleteTodo(id as number)}
         className={classNames(
           completed ? "" : "",
           "cursor-pointer overflow-hidden bg-red-800 p-1 text-sm text-white capitalize hover:bg-red-700/80 md:p-3",
