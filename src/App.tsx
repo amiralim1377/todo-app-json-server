83aa7a3c5b4737ccc8f19745f9c59a66af4e74ff
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 16:04:44 +0330

    change the layout base on width

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { AddTodoForm } from "./components/AddTodoForm/AddTodoForm";
 import { TodoHeader } from "./components/TodoHeader";
 import { TodoList } from "./components/TodoList";
+import { useWindowSize } from "./hook/useWindowSize";
+import { classNames } from "./utility/classNames";
 
 export default function App() {
+  const { width } = useWindowSize();
   return (
     <div className="">
-      <div className="relative bg-red-300">
+      <div className="relative">
         <TodoHeader />
-        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform xl:w-3/5">
-          <AddTodoForm />
-        </div>
+        {width >= 1024 && (
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform xl:w-3/5">
+            <AddTodoForm />
+          </div>
+        )}
       </div>
-      <div className="mx-auto mt-14 flex w-full flex-col items-center justify-center p-2 md:p-5 xl:w-2/3">
+      <div
+        className={classNames(
+          width > 1024 ? "mt-14" : "mt-2",
+          "mx-auto flex w-full flex-col items-center justify-center p-2 md:p-5 xl:w-2/3",
+        )}
+      >
+        {width < 1024 && <AddTodoForm />}
         <TodoList />
       </div>
     </div>
