bb3e5fc3090d29358039b30243074cce99154780
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 19:05:01 +0330

    add handleOpenModal and handleCloseModal

--- src/components/TaskListItem/index.tsx
+++ src/components/TaskListItem/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useWindowSize } from "../../hook/useWindowSize";
 import {
   doneHandler,
@@ -8,12 +9,15 @@ import type { ITodo } from "../../types/Todo.interface";
 import { classNames } from "../../utility/classNames";
 import { getRandomEmoji } from "../../utility/getRandomEmoji";
 import { getTimeDifference } from "../../utility/getTimeDifference";
+import NewCustomModal from "../ui/newCustomModal";
+import { EditTodoForm } from "../EditTodoForm";
 
 interface TaskListItemProps {
   todoItem: ITodo;
 }
 
 function TaskListItem({ todoItem }: TaskListItemProps) {
+  const [openModal, setOpenModal] = useState(false);
   const { completed, dueDate, id, todo } = todoItem;
   const { width } = useWindowSize();
 
@@ -44,64 +48,88 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
     }
   };
 
+  const handleOpenModal = (todoId: number) => {
+    const newUrl = new URLSearchParams();
+    newUrl.set("id", String(todoId));
+    window.history.pushState(null, "", `?${newUrl.toString()}`);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    window.history.replaceState({}, "", window.location.pathname);
+    setOpenModal(false);
+  };
+
   return (
-    <div
-      className={classNames(
-        completed
-          ? "border-gray-3000 cursor-default border bg-gray-200 text-gray-500"
-          : "",
-        "mt-1 flex overflow-hidden rounded-xl border border-zinc-300",
-      )}
-    >
-      <div className="flex-1 p-1 text-xs capitalize sm:text-sm md:p-3">
-        <span>{todo}</span>
-        <span>-</span>
-        <span className="font-bold">
-          {completed
-            ? `you did it ${getRandomEmoji()} `
-            : width > 320
-              ? "you must do it until: "
-              : "until:"}
-        </span>
-        <span>{completed ? "" : getTimeDifference(dueDate)}</span>
-      </div>
-      {!completed && (
+    <>
+      <div
+        className={classNames(
+          completed
+            ? "border-gray-3000 cursor-default border bg-gray-200 text-gray-500"
+            : "",
+          "mt-1 flex overflow-hidden rounded-xl border border-zinc-300",
+        )}
+      >
+        <div className="flex-1 p-1 text-xs capitalize sm:text-sm md:p-3">
+          <span>{todo}</span>
+          <span>-</span>
+          <span className="font-bold">
+            {completed
+              ? `you did it ${getRandomEmoji()} `
+              : width > 320
+                ? "you must do it until: "
+                : "until:"}
+          </span>
+          <span>{completed ? "" : getTimeDifference(dueDate)}</span>
+        </div>
+        {!completed && (
+          <button
+            onClick={() => handleDoneTodo(id as number)}
+            disabled={completed}
+            className={classNames(
+              completed
+                ? "border border-t-transparent border-b-transparent bg-gray-200 text-gray-500"
+                : "bg-amber-300 text-wrap text-black hover:bg-amber-200",
+              "cursor-pointer p-1 text-xs capitalize transition-all md:p-3",
+            )}
+          >
+            {isCompleted}
+          </button>
+        )}
+
         <button
-          onClick={() => handleDoneTodo(id as number)}
           disabled={completed}
           className={classNames(
             completed
-              ? "border border-t-transparent border-b-transparent bg-gray-200 text-gray-500"
-              : "bg-amber-300 text-wrap text-black hover:bg-amber-200",
-            "cursor-pointer p-1 text-xs capitalize transition-all md:p-3",
+              ? "cursor-not-allowed border-l border-l-zinc-500 bg-gray-200 text-gray-500"
+              : "bg-accent hover:bg-accent/80 cursor-pointer text-white",
+            "p-1 text-sm capitalize md:p-3",
+          )}
+          onClick={() => handleOpenModal(id as number)}
+        >
+          edit
+        </button>
+
+        <button
+          onClick={() => handleDeleteTodo(id as number)}
+          className={classNames(
+            completed ? "" : "",
+            "cursor-pointer overflow-hidden bg-red-800 p-1 text-sm text-white capitalize hover:bg-red-700/80 md:p-3",
           )}
         >
-          {isCompleted}
+          delete
         </button>
+      </div>
+      {openModal && (
+        <NewCustomModal
+          isOpen={openModal}
+          title="edit"
+          onClose={handleCloseModal}
+        >
+          <EditTodoForm />
+        </NewCustomModal>
       )}
-
-      <button
-        disabled={completed}
-        className={classNames(
-          completed
-            ? "cursor-not-allowed border-l border-l-zinc-500 bg-gray-200 text-gray-500"
-            : "bg-accent hover:bg-accent/80 cursor-pointer text-white",
-          "p-1 text-sm capitalize md:p-3",
-        )}
-      >
-        edit
-      </button>
-
-      <button
-        onClick={() => handleDeleteTodo(id as number)}
-        className={classNames(
-          completed ? "" : "",
-          "cursor-pointer overflow-hidden bg-red-800 p-1 text-sm text-white capitalize hover:bg-red-700/80 md:p-3",
-        )}
-      >
-        delete
-      </button>
-    </div>
+    </>
   );
 }
 
