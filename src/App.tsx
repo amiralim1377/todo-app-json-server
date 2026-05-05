4bdc655716ed38856d31434c13770d28943411fc
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 15:43:22 +0330

    change the position of Form

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import { TodoList } from "./components/TodoList";
 export default function App() {
   return (
     <div className="">
-      <TodoHeader />
-      <div className="mx-auto flex w-full flex-col items-center justify-center p-2 md:p-5 xl:w-2/3">
-        <AddTodoForm />
+      <div className="relative bg-red-300">
+        <TodoHeader />
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform xl:w-3/5">
+          <AddTodoForm />
+        </div>
+      </div>
+      <div className="mx-auto mt-14 flex w-full flex-col items-center justify-center p-2 md:p-5 xl:w-2/3">
         <TodoList />
       </div>
     </div>
