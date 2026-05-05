8a9b2975396f2a1bc62cc36ffc4fd7a6906408c1
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 19:52:13 +0330

    refactore the code to more readability

--- src/components/TaskListItem/index.tsx
+++ src/components/TaskListItem/index.tsx
@@ -1,73 +1,42 @@
-import { useState } from "react";
 import { useWindowSize } from "../../hook/useWindowSize";
-import {
-  doneHandler,
-  getTodo,
-  removeTodo,
-} from "../../services/Axios/Requests/todos/Todo";
+
 import type { ITodo } from "../../types/Todo.interface";
 import { classNames } from "../../utility/classNames";
 import { getRandomEmoji } from "../../utility/getRandomEmoji";
 import { getTimeDifference } from "../../utility/getTimeDifference";
 import NewCustomModal from "../ui/newCustomModal";
 import { EditTodoForm } from "../EditTodoForm";
+import { isCompleted } from "./utility/isCompleted";
+import { useTaskActions } from "./custom-hook/useTaskActions";
 
-interface TaskListItemProps {
+export interface TaskListItemProps {
   todoItem: ITodo;
 }
 
 function TaskListItem({ todoItem }: TaskListItemProps) {
-  const [openModal, setOpenModal] = useState(false);
   const { completed, dueDate, id, todo } = todoItem;
   const { width } = useWindowSize();
+  const {
+    handleCloseModal,
+    handleOpenModal,
+    handleDeleteTodo,
+    handleDoneTodo,
+    openModal,
+  } = useTaskActions(todoItem);
 
-  const isCompleted = completed
-    ? "Did it"
-    : width < 500
-      ? "Done"
-      : "mark as Done";
-
-  const handleDeleteTodo = async (id: number) => {
-    try {
-      await removeTodo(id);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleDoneTodo = async (todoId: number) => {
-    try {
-      const { data: todoData } = await getTodo();
-      const lookingTodo = todoData.find((todo: ITodo) => todo.id == todoId);
-      if (lookingTodo) {
-        const newTodo = { ...lookingTodo, completed: true };
-        await doneHandler(todoId, newTodo);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleOpenModal = (todoId: number) => {
-    const newUrl = new URLSearchParams();
-    newUrl.set("id", String(todoId));
-    window.history.pushState(null, "", `?${newUrl.toString()}`);
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    window.history.replaceState({}, "", window.location.pathname);
-    setOpenModal(false);
-  };
+  // Helper variables for cleaner tsx
+  const isMobile = width <= 320;
+  const baseContainerClass =
+    "mt-1 flex overflow-hidden rounded-xl border border-zinc-300";
+  const completedContainerClass =
+    "border-gray-300 cursor-default bg-gray-200 text-gray-500";
 
   return (
     <>
       <div
         className={classNames(
-          completed
-            ? "border-gray-3000 cursor-default border bg-gray-200 text-gray-500"
-            : "",
-          "mt-1 flex overflow-hidden rounded-xl border border-zinc-300",
+          baseContainerClass,
+          completed ? completedContainerClass : "",
         )}
       >
         <div className="flex-1 p-1 text-xs capitalize sm:text-sm md:p-3">
@@ -76,9 +45,9 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
           <span className="font-bold">
             {completed
               ? `you did it ${getRandomEmoji()} `
-              : width > 320
-                ? "you must do it until: "
-                : "until:"}
+              : isMobile
+                ? "until:"
+                : "you must do it until: "}
           </span>
           <span>{completed ? "" : getTimeDifference(dueDate)}</span>
         </div>
@@ -93,7 +62,7 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
               "cursor-pointer p-1 text-xs capitalize transition-all md:p-3",
             )}
           >
-            {isCompleted}
+            {isCompleted(completed, width)}
           </button>
         )}
 
@@ -125,6 +94,7 @@ function TaskListItem({ todoItem }: TaskListItemProps) {
           isOpen={openModal}
           title="edit"
           onClose={handleCloseModal}
+          className="w-full md:max-w-225"
         >
           <EditTodoForm />
         </NewCustomModal>
