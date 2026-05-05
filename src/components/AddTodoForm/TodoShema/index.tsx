584d1c75649b2515197e48afc69da08e0be3df3f
AMIRALI <alimoradi7718@gmail.com>
2026-05-05 19:52:35 +0330

    add more validation

--- src/components/AddTodoForm/TodoShema/index.tsx
+++ src/components/AddTodoForm/TodoShema/index.tsx
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 const todoSchema = z.object({
-  todo: z.string().min(1, { message: "you should enter your Todo first." }),
+  todo: z
+    .string()
+    .min(1, { message: "you should enter your Todo first." })
+    .max(20, { message: "at least 20 characters" }),
   dueDate: z.string().min(1, { message: "just select a day" }),
 });
 
