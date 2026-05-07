94ae78d610acd805100a4ad50db3a4c43a6790bf
AMIRALI <alimoradi7718@gmail.com>
2026-05-07 15:19:54 +0330

    add mainProvider to manage app with context

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { MainProvider } from "./provider/mainProvider/index.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <MainProvider>
+    <App />
+  </MainProvider>,
+);
