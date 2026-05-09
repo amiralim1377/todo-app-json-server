4e9ab0145478d873419d313a4e81f16a77b43edf
AMIRALI <alimoradi7718@gmail.com>
2026-05-09 11:08:58 +0330

    new useTaskActions

---
+++ src/hook/useTaskActions/index.tsx
@@ -0,0 +1,55 @@
+import { useModalContext } from "../../context/ModalContext/ModalContext";
+import type { ModalStateType } from "../../context/ModalContext/ModalContextProvider";
+import { useTodoContext } from "../../context/TodoContext/todoContext";
+import {
+  doneHandler,
+  removeTodo,
+} from "../../services/Axios/Requests/todos/Todo";
+import type { ITodo } from "../../types/Todo.interface";
+
+const useTaskActions = (todoItem: ITodo) => {
+  const { setTodoArray } = useTodoContext();
+  const { openModal, closeModal } = useModalContext();
+
+  const handleDoneTodo = async (todoId: number) => {
+    try {
+      const newTodo = { ...todoItem, completed: true };
+      await doneHandler(todoId as number, newTodo);
+      setTodoArray((prev) =>
+        prev.map((todo) => (todo.id === todoId ? newTodo : todo)),
+      );
+    } catch (error) {
+      console.error("Failed to complete todo:", error);
+    }
+  };
+
+  const handleDeleteTodo = async (id: number) => {
+    try {
+      await removeTodo(id as number);
+      setTodoArray((prev) => prev.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error("Failed to delete todo:", error);
+    }
+  };
+
+  const handleOpenModal = (todoId: number, type: ModalStateType) => {
+    const newUrl = new URLSearchParams();
+    newUrl.set("id", String(todoId));
+    window.history.pushState(null, "", `?${newUrl.toString()}`);
+    openModal(todoId, type);
+  };
+
+  const handleCloseModal = () => {
+    window.history.replaceState({}, "", window.location.pathname);
+    closeModal();
+  };
+
+  return {
+    handleCloseModal,
+    handleOpenModal,
+    handleDeleteTodo,
+    handleDoneTodo,
+  };
+};
+
+export { useTaskActions };
