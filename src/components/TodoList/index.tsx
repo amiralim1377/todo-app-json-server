db83b93d28536fb8d7eea73e4d0cc4ae132643d1
AMIRALI <alimoradi7718@gmail.com>
2026-05-09 11:03:34 +0330

    add scrol  style to TodoList components

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -2,16 +2,18 @@ import { TaskListItem } from "../TaskListItem";
 import type { ITodo } from "../../types/Todo.interface";
 import { NoTasks } from "../NoTasks";
 import { useTodoContext } from "../../context/TodoContext/todoContext";
+import styles from "./index.module.css";
+import { classNames } from "../../utility/classNames";
 
 function TodoList() {
   const { todos } = useTodoContext();
 
   return (
     <div className="mt-1 w-full">
-      <h1 className="text-left text-xl text-zinc-600 capitalize md:text-4xl">
+      <h1 className="mb-1.5 border-b border-zinc-300 text-left text-xl text-zinc-600 capitalize md:text-4xl">
         task
       </h1>
-      <div className="max-h-72 overflow-y-scroll">
+      <div className={classNames(styles.scrol, "max-h-72 overflow-y-scroll")}>
         {todos.length > 0 ? (
           todos?.map((todoItem: ITodo) => {
             return <TaskListItem key={todoItem.id} todoItem={todoItem} />;
