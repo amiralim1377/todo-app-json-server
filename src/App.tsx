4674d2b25eb9043c0d598ffc2f4c8c27e8abc9e6
AMIRALI <alimoradi7718@gmail.com>
2026-05-09 11:00:04 +0330

    add some property base on layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ import { classNames } from "./utility/classNames";
 export default function App() {
   const { width } = useWindowSize();
   return (
-    <div className="">
+    <div className="h-screen w-full">
       <div className="relative">
         <TodoHeader />
         {width >= 1024 && (
-          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform xl:w-3/5">
+          <div className="absolute bottom-0 left-1/2 w-2/3 -translate-x-1/2 translate-y-1/2 transform">
             <AddTodoForm />
           </div>
         )}
       </div>
       <div
         className={classNames(
-          width > 1024 ? "mt-14" : "mt-2",
+          width > 1024 ? "mt-14" : "mt-8",
           "mx-auto flex w-full flex-col items-center justify-center p-2 md:p-5 xl:w-2/3",
         )}
       >
