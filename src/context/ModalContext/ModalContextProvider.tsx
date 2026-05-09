dfbf3865d761731f18fda1ab2023b0fb669bb509
AMIRALI <alimoradi7718@gmail.com>
2026-05-09 11:04:51 +0330

    remove activeTodoId state and mange it with just one-state base on id and type

--- src/context/ModalContext/ModalContextProvider.tsx
+++ src/context/ModalContext/ModalContextProvider.tsx
@@ -2,28 +2,36 @@ import { useState } from "react";
 
 import { ModalContext } from "./ModalContext";
 
+export type ModalStateType = "edit" | "delete" | null;
+
+export type ModalStateProps = {
+  isOpen: boolean;
+  type: ModalStateType;
+  id: number | null;
+};
+
 export default function ModalContextProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [activeTodoId, setActiveTodoId] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<ModalStateProps>({
+    isOpen: false,
+    type: null,
+    id: null,
+  });
 
-  const openModal = (id: number) => {
-    setIsModalOpen(true);
-    setActiveTodoId(id);
+  const openModal = (modalId: number, type: ModalStateType) => {
+    setIsModalOpen({ isOpen: true, id: modalId, type });
   };
   const closeModal = () => {
-    setIsModalOpen(false);
-    setActiveTodoId(null);
+    setIsModalOpen({ isOpen: false, id: null, type: null });
   };
 
   const value = {
     openModal,
     closeModal,
     isModalOpen,
-    activeTodoId,
   };
 
   return (
